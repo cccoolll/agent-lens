bcefac9f4215ab095907d616ce596e2d8dc298e6
Hugo Dettner Källander <hugodk@kth.se>
2024-12-02 16:09:50 +0100

    Refactor: move scripts/ back to src/

--- scripts/run_dev.sh
+++
@@ -1,6 +0,0 @@
-python agent_lens/register-sam-service.py &
-python agent_lens/register-similarity-search-service.py &
-cd agent_lens/static
-npm run start
-
-wait
---
+++ src/scripts/run_dev.sh
@@ -0,0 +1,6 @@
+python src/backend/register-sam-service.py &
+python src/backend/register-similarity-search-service.py &
+cd src/frontend
+npm run start
+
+wait
