8d507cad0fd063f86a1e9ea9ec4d3685bb3c4160
cccoolll <36098954+cccoolll@users.noreply.github.com>
2024-12-02 15:48:57 +0100

    Refactor requirements and clean up unused database functions

--- scripts/register-similarity-search-service.py
+++ scripts/register-similarity-search-service.py
@@ -21,9 +21,6 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
 # Connect to the SQLite database
-def get_db_connection():
-    conn = sqlite3.connect('image_vectors-hpa.db')
-    return conn, conn.cursor()
 
 def get_cell_db_connection():
   conn = sqlite3.connect('cell_vectors_db.db')
@@ -39,31 +36,6 @@ def get_cell_db_connection():
   ''')
   return conn, conn.cursor()
 
-def load_vectors_from_db(channel=None):
-    conn, c = get_db_connection()
-    query = 'SELECT id, vector, image_path, fluorescent_channel FROM images'
-    if channel:
-        query += f" WHERE fluorescent_channel = '{channel}'"
-    c.execute(query)
-    rows = c.fetchall()
-    conn.close()
-
-    image_ids = []
-    image_vectors = []
-    image_paths = {}
-    image_channels = {}
-
-    for row in rows:
-        img_id, img_vector, img_path, img_channel = row
-        img_vector = np.frombuffer(img_vector, dtype=np.float32)
-        image_ids.append(img_id)
-        image_vectors.append(img_vector)
-        image_paths[img_id] = img_path
-        image_channels[img_id] = img_channel
-    print(f"Loaded {len(image_ids)} image vectors")
-    print(f"Image vector shape: {image_vectors[0].shape}")
-    
-    return image_ids, np.array(image_vectors), image_paths, image_channels
 
 def load_cell_vectors_from_db():
     conn, c = get_cell_db_connection()
@@ -94,90 +66,6 @@ def build_faiss_index(vectors):
     index.add(vectors.astype(np.float32))
     return index
 
-
-# Load vectors and build FAISS index based on the channel
-image_ids, image_vectors, image_paths, image_channels = load_vectors_from_db()
-print(f"types: {type(image_ids)}, {type(image_vectors)}, {type(image_paths)}, {type(image_channels)}")
-
-index = build_faiss_index(image_vectors)
-
-"""
-Precompute separate FAISS indices for each channel at the time of building the indices.
-Use the appropriate index based on the channel extracted from the input image name.
-"""
-
-def separate_indices_by_channel(image_vectors, image_channels):
-    indices = {}
-    image_channel_info= image_channels.values()
-    #channel_vectors =
-    for channel in set(image_channel_info):
-        channel_vectors = [image_vectors[i] for i, c in enumerate(image_channel_info) if c == channel]
-        indices[channel] = build_faiss_index(np.array(channel_vectors))
-        print(f"Built index for channel: {channel},the legnth of channel_vectors is {len(channel_vectors)}")
-    return indices
-
-channel_indices = separate_indices_by_channel(image_vectors, image_channels)
-
-def find_similar_images(input_image,image_data, top_k=5, index=index):
-    input_image_name=image_data['name']
-    try:
-        channel = None
-        if '-' in input_image_name:
-            'image-green.png'
-            ['image','green.png']
-            'green.png'
-            ['green', 'png']
-            'green'
-             
-            channel = input_image_name.split('-')[1].split('.')[0]
-
-        # Convert input bytes to an image
-        image = Image.open(io.BytesIO(input_image)).convert("RGB")
-
-        # Process the image
-        image_input = preprocess(image).unsqueeze(0).to(device)
-        with torch.no_grad():
-            query_vector = model.encode_image(image_input).cpu().numpy().flatten()
-
-        print(f"Query vector shape: {query_vector.shape}")
-        
-        query_vector = np.expand_dims(query_vector, axis=0).astype(np.float32)
-
-
-        distances, indices = index.search(query_vector, len(image_ids))  # Search all images
-
-        # Collect and sort results
-        results = []
-        for i, idx in enumerate(indices[0]):
-            img_id = image_ids[idx]
-            distance = distances[0][i]
-            sim = 1 - distance  # Convert distance to similarity
-            print(f"ID: {img_id}, Score: {sim:.2f}")
-            print(f"Image path: {image_paths[img_id]}")
-            print(f"Fluorescent channel: {image_channels[img_id]}")
-            print("---")
-            
-            # Open the image, resize it, and convert to base64
-            with Image.open(image_paths[img_id]) as img:
-                img.thumbnail((512, 512))  # Resize image to max 512x512 while maintaining aspect ratio
-                buffered = io.BytesIO()
-                img.save(buffered, format="JPEG")
-                img_str = base64.b64encode(buffered.getvalue()).decode()
-            
-            results.append({
-                'image': img_str,
-                'image_path': image_paths[img_id],
-                'fluorescent_channel': image_channels[img_id],
-                'similarity': float(sim)
-            })
-            if len(results) >= top_k:
-                break
-        
-        return results
-    except Exception as e:
-        print(f"Error processing image: {e}")
-        traceback.print_exc()
-        return []
   
 def find_similar_cells(input_cell_image, original_filename=None, top_k=5):
   try:
@@ -234,50 +122,6 @@ def find_similar_cells(input_cell_image, original_filename=None, top_k=5):
       traceback.print_exc()
       return {"status": "error", "message": str(e)}
   
-def add_image_to_db(image_bytes, image_name, image_channel, image_folder='images'):
-  try:
-      # Ensure the image folder exists
-      os.makedirs(image_folder, exist_ok=True)
-
-      # Save the image to the specified folder
-      image_path = os.path.join(image_folder, image_name)
-      with open(image_path, 'wb') as f:
-          f.write(image_bytes)
-
-      # Open and preprocess the image
-      image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-      image_input = preprocess(image).unsqueeze(0).to(device)
-
-      # Compute the image embedding
-      with torch.no_grad():
-          image_vector = model.encode_image(image_input).cpu().numpy().flatten()
-
-      # Connect to the database and insert the new image data
-      conn, c = get_db_connection()
-      c.execute(
-          'INSERT INTO images (vector, image_path, fluorescent_channel) VALUES (?, ?, ?)',
-          (image_vector.tobytes(), image_path, image_channel)
-      )
-      conn.commit()
-      conn.close()
-
-      # Update the FAISS index
-      global image_ids, image_vectors, image_paths, image_channels, index, channel_indices
-      image_ids.append(c.lastrowid)
-      image_vectors = np.vstack([image_vectors, image_vector])
-      image_paths[c.lastrowid] = image_path
-      image_channels[c.lastrowid] = image_channel
-
-      # Rebuild the FAISS index
-      index = build_faiss_index(image_vectors)
-      channel_indices = separate_indices_by_channel(image_vectors, image_channels)
-
-      print(f"Image {image_name} added successfully.")
-      return {"status": "success", "message": f"Image {image_name} added successfully."}
-  except Exception as e:
-      print(f"Error adding image: {e}")
-      traceback.print_exc()
-      return {"status": "error", "message": str(e)}
 
 def save_cell_image(cell_image, mask=None, annotation=""):
   try:
@@ -325,8 +169,6 @@ async def start_hypha_service(server):
                 "require_context": False,   
             },
             "type": "echo",
-            "find_similar_images": find_similar_images,
-            "add_image_to_db": add_image_to_db,
             "find_similar_cells": find_similar_cells,
             "save_cell_image": save_cell_image,
         },
