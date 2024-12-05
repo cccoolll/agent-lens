baaf41bbc38ac9597b3343d760f26a502de1224d
Hugo Dettner Källander <hugodk@kth.se>
2024-12-05 13:47:17 +0100

    Fix dockerfile, button size

--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+npm install --prefix src/frontend
 vips dzsave src/frontend/img/example_image.png src/frontend/tiles_output --layout google
 python src/backend/rebuild_cell_db_512.py
-python src/backend/embed_image_vectors.py --image_folder src/frontend/tiles_output --datatype squid
+python src/backend/embed-image-vectors.py --image_folder src/frontend/tiles_output --datatype squid
 mv src/frontend/tiles_output/1/1/1.jpg src/frontend/tiles_output/1/1/1.bmp
