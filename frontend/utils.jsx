fd8259f311d9e56b7c83cfedb4c85487e5468441
Songtao Cheng <songtao.cheng@scilifelab.se>
2025-04-02 15:50:06 +0200

    Update service reference in initializeServices function to use Kubernetes simulation endpoint for microscope control.

--- frontend/utils.jsx
+++ frontend/utils.jsx
@@ -40,7 +40,7 @@ export const initializeServices = async (
   const microscopeControlService = await tryGetService(
     server,
     "Microscope Control",
-    "squid-control/microscope-control-squid-simulation-reef",
+    "squid-control/microscope-control-squid-simulation-k8s",
     null,
     appendLog
   );
